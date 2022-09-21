eab7714d0e5cfa039adc91d8678324317bd181ac
Sabryna <sabrynadeangelo@gmail.com>
2022-09-21 09:26:09 -0300

    Criando projeto Cadeiras

---
+++ Projeto Cadeira/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Projeto_Cadeira
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá bem vindo!");
+            Console.WriteLine("Digite 1 para listar nossas opçoes de cadeiras ");
+            Console.WriteLine("Se deseja sair, digite 0");
+            int opUser = int.Parse(Console.ReadLine());
+
+            Cadeira Escritorio = new Cadeira("Escritorio", "Preta", "5 Rodas", "5 Anos");
+            Cadeira Gamer = new Cadeira("Gamer", "Azul", "6 Rodas", "3 Anos");
+            Cadeira UniWhite = new Cadeira("Uni White", "Cinza", "5 Rodas", "4 Anos");
+            Cadeira RollerWhite = new Cadeira("Roller White", "Branca", "5 Rodas", "5 Anos");
+
+            Console.WriteLine(Escritorio.ToString());
+            Console.WriteLine(Gamer.ToString());
+            Console.WriteLine(UniWhite.ToString());
+            Console.WriteLine(RollerWhite.ToString());
+            Console.WriteLine("\nAplicativo finalizado\n");
+        }
+    }
+}
---
+++ Projeto Cadeira/src/Cadeira.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Projeto_Cadeira
+{
+    public class Cadeira
+    {
+
+        public string Modelo { get; set; }
+        public string Cor { get; set; }
+        public string Rodas { get; set; }
+        public string Garantia { get; set; }
+
+        public Cadeira()
+        {
+        }
+
+        public Cadeira(string modelo, string cor, string rodas, string garantia)
+        {
+            Modelo = modelo;
+            Cor = cor;
+            Rodas = rodas;
+            Garantia = garantia;
+
+        }
+        public override string ToString()
+        {
+            return $"Modelo: {Modelo};\n" +
+                $"Cor: {Cor};\n" +
+                $"Rodas: {Rodas};\n" +
+                $"Garantia: {Garantia};\n";
+        }
+    }
+}
+
